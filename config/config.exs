c9a1e7cfd6ac921f56cba681d62cdfa2a5dbe5c3
Ahmad Albu-Eltyif <ahmadeltyif@gmail.com>
2018-05-21 01:39:08 +0300

    Discuss Project for elixir phoenix bootcamp

---
+++ config/config.exs
@@ -0,0 +1,35 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "yLEcNXPLK/1izCAgpWWhPRtRcLj3dTJn0u8Hg4z+ruzBckrx18um4yXAuzBXuXP1",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "37013371afe8a8c7e3e4",
+  client_secret: "b4c7f91d14a5f6ae8e8deeb21c124c88dcd6cdd5"
